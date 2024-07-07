19582d4fea8ec13caa9100e4547141c18af65d5b
Abhisekh Upadhaya <Abhisek0721@gmail.com>
2024-07-07 21:46:06 +0530

    refactor: moved signup and user api to auth module

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+# users/views.py
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+from authentication.serializers import RegisterSerializer, LoginSerializer
+from identity_service_creato.utils.api_response import api_response
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    @swagger_auto_schema(
+        request_body=RegisterSerializer,
+        responses={status.HTTP_201_CREATED: "User registered successfully"},
+        operation_description="Register a new user."
+    )
+    def post(self, request, *args, **kwargs):
+        try:
+            serializer = RegisterSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                data = serializer.save()
+                return api_response(data=data, message="Successful signup response.", status=status.HTTP_201_CREATED)
+        except Exception as e:
+            print(e)
+            return api_response(data=None, message="Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+
+class LoginView(APIView):
+    permission_classes = (AllowAny,)
+    @swagger_auto_schema(
+        request_body=LoginSerializer,
+        responses={
+            status.HTTP_200_OK: openapi.Response("Successful login response"),
+        },
+        operation_description="Login with username and password."
+    )
+    def post(self, request, *args, **kwargs):
+        try:
+            serializer = LoginSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                validated_data = serializer.validated_data
+                response_data = {
+                    "access_token": validated_data["access_token"],
+                    "user": validated_data["user"]
+                }
+                return api_response(data=response_data, message="Login successful.", status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
+            return api_response(data=None, message="Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
