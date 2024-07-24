79c32abf3b784876e558048c480fee27c095ac1f
Abhisekh Upadhaya <Abhisek0721@gmail.com>
2024-07-24 22:06:03 +0530

    feat: resend verification email and verify user api

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,13 @@
 # users/urls.py
 from django.urls import path, include
-from authentication.views import RegisterView, LoginView
+from authentication.views import RegisterView, LoginView, ResendVerificationEmailView, VerifyUserView
 
 
 user_patterns = [
     path('signup', RegisterView.as_view(), name='signup'),
     path('login', LoginView.as_view(), name='login'),
+    path('resend-verification-email', ResendVerificationEmailView.as_view(), name='resend_verification_email'),
+    path('verify-user', VerifyUserView.as_view(), name="verify_user")
 ]
 
 urlpatterns = [
